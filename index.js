46efecc267e66ce9485e47188dc609db2f505f01
Nazar Kruk <nazarkruk@icloud.com>
2022-01-16 16:24:41 -0500

    Done

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(x, y){
+    return x+y
+}
+function subtract(x,y){
+    return x-y
+}
+function multiply(x, y){
+    return x*y
+}
+function divide(a, b){
+    return a/b
+}
+function increment(n){
+    return (n+=1)
+}
+function decrement(n){
+    return (n-=1)
+}
+function makeInt(string){
+    return parseInt(string, 10)
+}
+function preserveDecimal(string){
+    return parseFloat(string)
+}
